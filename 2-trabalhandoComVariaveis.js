5a581b011a1f7475adb52003e7a2167ca8fdde72
Gisele Faria <gisodre.sda@outlook.com>
2021-07-05 22:23:02 -0300

    Atualizando o arquivo 2 com uma informação extra sobre variáveis

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -3,3 +3,4 @@ console.log("Trabalhando com variáveis");
 
 const idade = 24;
 const nome = "Gisele";
+// let idade = 28; - Essa, é uma outra forma de estabelecer variáveis, mas falaremos disso mais para frente.
