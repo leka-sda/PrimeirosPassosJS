5a865dbaff9b01a757d9e2212f0ce3d8ac528220
Gisele Faria <gisodre.sda@outlook.com>
2021-07-05 21:18:53 -0300

    Primeiro commit

---
+++ MeuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 34;
+
+const idadeSomada = idade+2;
+const idadeSubtraida = idade-2;
+const idadeDividida = idade/2;
+const idadeMultiplicada = idade*2;
+
+
+console.log("Minha idade é: ", idade);
+console.log("Idade Somada é: ", idadeSomada);
+console.log("Idade Subtraída é: ", idadeSubtraida);
+console.log("Idade Dividida é: ", idadeDividida);
+console.log("Idade Multiplicada é: ", idadeMultiplicada);
